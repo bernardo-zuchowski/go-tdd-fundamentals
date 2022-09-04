248cc82312daad78813391287f29268fd246858a
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2022-09-04 17:46:31 -0300

    feat: implement bitcoin deposit and balance

--- pointer_errors/wallet_test.go
+++ pointer_errors/wallet_test.go
@@ -3,14 +3,29 @@ package pointer_errors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	w := Wallet{}
+	t.Run("deposit", func(t *testing.T) {
+		w := Wallet{}
 
-	w.Deposit(Bitcoin(10))
+		w.Deposit(Bitcoin(10))
 
-	got := w.Balance()
-	want := Bitcoin(10)
+		got := w.Balance()
+		want := Bitcoin(10)
 
-	if got != want {
-		t.Errorf("got %s want %s", got, want)
-	}
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("deposit", func(t *testing.T) {
+		w := Wallet{Bitcoin(20)}
+
+		w.Withdraw(Bitcoin(10))
+
+		got := w.Balance()
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
