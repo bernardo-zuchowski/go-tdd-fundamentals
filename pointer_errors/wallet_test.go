052a9c7d8908c8a1202aea2898e47a463d05c13c
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2022-09-04 17:54:32 -0300

    refactor: unifies repeated assertions

--- pointer_errors/wallet_test.go
+++ pointer_errors/wallet_test.go
@@ -2,30 +2,28 @@ package pointer_errors
 
 import "testing"
 
+var assertBalance = func(t testing.TB, w Wallet, want Bitcoin) {
+	t.Helper()
+
+	got := w.Balance()
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
+
 func TestWallet(t *testing.T) {
 	t.Run("deposit", func(t *testing.T) {
 		w := Wallet{}
-
 		w.Deposit(Bitcoin(10))
 
-		got := w.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, w, Bitcoin(10))
 	})
 
-	t.Run("deposit", func(t *testing.T) {
-		w := Wallet{Bitcoin(20)}
-
+	t.Run("withdraw", func(t *testing.T) {
+		w := Wallet{10}
 		w.Withdraw(Bitcoin(10))
 
-		got := w.Balance()
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, w, Bitcoin(0))
 	})
 }
