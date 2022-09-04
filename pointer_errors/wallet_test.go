e15c7280f4da3bf89da74c6eba316427a0efd021
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2022-09-04 17:45:05 -0300

    feat: implement bitcoin deposit and balance

---
+++ pointer_errors/wallet_test.go
@@ -0,0 +1,16 @@
+package pointer_errors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	w := Wallet{}
+
+	w.Deposit(Bitcoin(10))
+
+	got := w.Balance()
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %s want %s", got, want)
+	}
+}
