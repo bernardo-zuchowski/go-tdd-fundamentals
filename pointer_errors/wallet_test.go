4d9134612594c4f6c2b89bd4dbfe5116745e07ab
Bernardo Zuchowski <ber.s.zuc@gmail.com>
2022-09-05 13:13:53 -0300

    feat: add functions and tests for pointers and errors

--- pointer_errors/wallet_test.go
+++ pointer_errors/wallet_test.go
@@ -2,13 +2,30 @@ package pointer_errors
 
 import "testing"
 
-var assertBalance = func(t testing.TB, w Wallet, want Bitcoin) {
+func assertBalance(t testing.TB, w Wallet, want Bitcoin) {
 	t.Helper()
-
 	got := w.Balance()
 
 	if got != want {
-		t.Errorf("got %s want %s", got, want)
+		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
 	}
 }
 
@@ -20,10 +37,20 @@ func TestWallet(t *testing.T) {
 		assertBalance(t, w, Bitcoin(10))
 	})
 
-	t.Run("withdraw", func(t *testing.T) {
-		w := Wallet{10}
-		w.Withdraw(Bitcoin(10))
+	t.Run("withdraw with funds", func(t *testing.T) {
+		w := Wallet{Bitcoin(20)}
+		err := w.Withdraw(Bitcoin(10))
 
-		assertBalance(t, w, Bitcoin(0))
+		assertNoError(t, err)
+		assertBalance(t, w, Bitcoin(10))
+	})
+
+	t.Run("withdraw insufficient funds", func(t *testing.T) {
+		initialBalance := Bitcoin(20)
+		w := Wallet{initialBalance}
+		err := w.Withdraw(Bitcoin(100))
+
+		assertError(t, err, ErrInsufficientFunds)
+		assertBalance(t, w, initialBalance)
 	})
 }
